121c6e0095aa0826ce7303d290db645337a93643
dasith <dasiththisaru@gmail.com>
2024-09-27 13:31:51 +0530

    build the finallayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="red">
+        nav
+      </GridItem>
+      <Show above={"lg"}>
+        <GridItem area="aside" bg="yellow">
+          aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="orange">
+        main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
